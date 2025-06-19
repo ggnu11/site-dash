845d54fbd8f7111a8f936b7e913347b311e3295f
choi0712 <130032473+ggnu11@users.noreply.github.com>
2025-06-19 14:30:59 +0900

    원복

--- src/processes/routing/ui/AppRouter.tsx
+++ src/processes/routing/ui/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import {
   HashRouter as Router,
   Routes,
@@ -8,46 +8,37 @@ import {
 import { ProtectedRoute } from "@/processes/routing/lib/ProtectedRoute";
 import { AppRoutes } from "@/processes/routing/model/routes";
 
-// 동적 임포트를 사용한 페이지 컴포넌트 로딩
-const LoginPage = lazy(() => import("@/pages/LoginPage"));
-const SiteDash = lazy(() => import("@/SiteDash"));
-
-// 로딩 컴포넌트
-const Loader = () => (
-  <div className="flex justify-center items-center min-h-screen bg-[#1E1E1E]">
-    <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-yellow-500"></div>
-  </div>
-);
+// 페이지 컴포넌트 임포트
+import LoginPage from "@/pages/LoginPage";
+import SiteDash from "@/SiteDash";
 
 /** 애플리케이션 라우터 컴포넌트 */
 export const AppRouter: React.FC = () => {
   return (
     <Router>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          {/* 로그인 페이지 */}
-          <Route path={AppRoutes.LOGIN} element={<LoginPage />} />
+      <Routes>
+        {/* 로그인 페이지 */}
+        <Route path={AppRoutes.LOGIN} element={<LoginPage />} />
 
-          {/* 대시보드 (보호된 라우트) */}
-          <Route
-            path={AppRoutes.DASHBOARD}
-            element={
-              <ProtectedRoute>
-                <SiteDash />
-              </ProtectedRoute>
-            }
-          />
+        {/* 대시보드 (보호된 라우트) */}
+        <Route
+          path={AppRoutes.DASHBOARD}
+          element={
+            <ProtectedRoute>
+              <SiteDash />
+            </ProtectedRoute>
+          }
+        />
 
-          {/* 루트 경로 처리 */}
-          <Route
-            path={AppRoutes.ROOT}
-            element={<Navigate to={AppRoutes.LOGIN} replace />}
-          />
+        {/* 루트 경로 처리 */}
+        <Route
+          path={AppRoutes.ROOT}
+          element={<Navigate to={AppRoutes.LOGIN} replace />}
+        />
 
-          {/* 일치하는 라우트 없을 경우 */}
-          <Route path="*" element={<Navigate to={AppRoutes.LOGIN} replace />} />
-        </Routes>
-      </Suspense>
+        {/* 일치하는 라우트 없을 경우 */}
+        <Route path="*" element={<Navigate to={AppRoutes.LOGIN} replace />} />
+      </Routes>
     </Router>
   );
 };
